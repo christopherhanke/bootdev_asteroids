dbccc6ec4d654072cbc537be44f6aa5f7231405a
christopherhanke <christopherhanke@googlemail.com>
2025-01-31 17:40:09 +0100

    add constants to project

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
 
 def main():
     print("Starting asteroids!")
+    print("Screen width: " + str(SCREEN_WIDTH))
+    print("Screen height: " + str(SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     return
 
 if __name__ == "__main__":
