127973eed4b58b67606b76fd48a465b676de7c15
christopherhanke <christopherhanke@googlemail.com>
2025-02-01 17:15:59 +0100

    adding game loop and quit func

--- main.py
+++ main.py
@@ -5,8 +5,15 @@ def main():
     print("Starting asteroids!")
     print("Screen width: " + str(SCREEN_WIDTH))
     print("Screen height: " + str(SCREEN_HEIGHT))
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    return
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0))
+        pygame.display.flip()
+        
 
 if __name__ == "__main__":
     main()
