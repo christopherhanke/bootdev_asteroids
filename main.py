2ef69c5fe4ce977e2af102bb1b29fbc30c61242b
christopherhanke <christopherhanke@googlemail.com>
2025-02-08 17:07:18 +0100

    structuring game objects in groups

--- main.py
+++ main.py
@@ -10,14 +10,23 @@ def main():
     game_time = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+
     p1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
-        p1.draw(screen)
-        p1.update(dt)
+        updateable.update(dt)
+        for object in drawable:
+            object.draw(screen)
+
+        # p1.draw(screen)
+        # p1.update(dt)
         pygame.display.flip()
         dt = pygame.time.Clock.tick(game_time, 60) / 1000
         
